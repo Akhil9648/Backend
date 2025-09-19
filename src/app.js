edfbcbd78647d39c87998cb0c583f1b6d09a5fac
Akhil <akhilpandey494@gmail.com>
2025-09-19 13:37:19 +0530

    Api Integration

--- src/app.js
+++ src/app.js
@@ -9,4 +9,8 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+// routes
+import userRouter from './routes/user.routes.js'
+// rotes declaration
+app.use("/api/v1/users",userRouter)
 export {app} 
